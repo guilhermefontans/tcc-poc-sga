e024385ade5ae9d2b877da1f86f6719929cacab0
Guilherme Fontans <guilherme.fontans@gmail.com>
2020-03-05 11:38:52 -0300

    Adiocionando login por token nas apis

--- api-gateway/src/routes.js
+++ api-gateway/src/routes.js
@@ -2,7 +2,9 @@ const express  = require('express');
 const httpProxy = require('express-http-proxy');
 const app = express();
 const router = express.Router();
-
+const verifyJWT = require('./auth/verifyJWT');
+require("dotenv-safe").config();
+var jwt = require('jsonwebtoken');
 const cadastroAtivosServiceProxy = httpProxy('http://localhost:3334');
 const relatoriosServiceProxy = httpProxy('http://localhost:3333');
 
@@ -11,11 +13,23 @@ router.use((req, res, next) => {
     next();
 });
 
-router.use('/cadastro-ativos', (req, res, next) => {
+router.post('/login', (req, res, next) => {
+  //temporariamente usuario mocado  
+  if(req.body.user === 'fontans' && req.body.password === '123'){
+    const id = 1;
+    var token = jwt.sign({ id }, process.env.SECRET, {
+      expiresIn: 300
+    });
+    res.status(200).send({ auth: true, token: token });
+  }
+  res.status(500).send('Login inválido!');
+});
+
+router.use('/cadastro-ativos', verifyJWT, (req, res, next) => {
    cadastroAtivosServiceProxy(req, res, next);
 });
 
-router.use('/relatorios', (req, res, next) => {
+router.use('/relatorios', verifyJWT, (req, res, next) => {
   relatoriosServiceProxy(req, res, next);
 });
 
