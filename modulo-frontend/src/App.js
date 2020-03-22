229d897c0c266a44d24c53190d73acb12584fde5
Guilherme Fontans <guilherme.fontans@gmail.com>
2020-03-21 23:39:36 -0300

    Inicio do frontend

---
+++ modulo-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './components/login.component'
+
+function App() {
+  return (
+    <div className="App">
+        Login Page
+    </div>
+  );
+}
+
+export default App;
